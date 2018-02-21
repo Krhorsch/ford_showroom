59ee99dc11a6f793b9ebf8abdf9909b9028582c4
Kyle Horsch <krhorsch@yahoo.com>
2018-02-21 15:32:46 +0000

    scraper reworked

--- lib/ford_showroom/car.rb
+++ lib/ford_showroom/car.rb
@@ -1,5 +1,5 @@
 class FordShowroom::Car
-  attr_accessor :name, :info
+  attr_accessor :name, :info, :price
   @@all_cars = []
   def initialize
     @@all_cars << self
--- lib/ford_showroom/scraper.rb
+++ lib/ford_showroom/scraper.rb
@@ -2,32 +2,14 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 class FordShowroom::Scraper
-  def vehicle
+
+  def self.vehicle
     doc = Nokogiri::HTML(open("https://www.ford.com/new-cars/?gnav=footer-all%20vehicles"))
     cars = doc.css("div.single-static-feature-text")
     cars.each do |x|
-      x = FordShowroom::Car.new
-      x.name = x.css("b").text
-
-    #cars_array = cars.split(/\u00A0|\s/)
-    #cars_array.each do |car|
-    #  car = FordShowroom::Car.new
-    #  car.name = car
-    binding.pry
-      #puts "#{cars_array.index(car)+1}. #{car}"
+      new_car = FordShowroom::Car.new
+      new_car.name = x.css("b").text
+      new_car.info = x.css("span.description").text
+      new_car.price = x.css(".m-text-default .starting-price").text
     end
   end
-
-  def self.vehicle_info
-    doc = Nokogiri::HTML(open("https://www.ford.com/new-cars/?gnav=footer-all%20vehicles"))
-    cars = doc.css("div.single-static-feature-text")
-    cars.each do |car|
-      @@car_info << car.css(".m-text-default span").text
-    end
-    @@car_info
-  end
-
-  def self.all_car_info
-    @@car_info
-  end
-end
